6b662e9705ee021d919cc2ae752a18df7c50d493
mechtatell <kalachikovia@gmail.com>
2020-10-28 11:07:25 +0400

    lab4 rdy

--- Laboratories/Lab3/Main.java
+++
@@ -1,9 +0,0 @@
-package Lab3;
-
-public class Main {
-
-    public static void main(String[] args) {
-        MemoryManager memoryManager = new MemoryManager();
-        memoryManager.start();
-    }
-}
